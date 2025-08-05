6fc999d5231114177b68dc384074af9b71e684aa
Zuckareya Amin <aminzak1234@gmail.com>
2025-08-05 15:50:50 +0100

    terraform

--- main.tf
+++ main.tf
@@ -2,31 +2,28 @@ provider "aws" {
   region = "us-west-2"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# 🔑 Generate key pair and save locally
+resource "tls_private_key" "ec2_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-  }
+resource "aws_key_pair" "ec2_key" {
+  key_name   = "my-ec2-key"
+  public_key = tls_private_key.ec2_key.public_key_openssh
+}
 
-  owners = ["099720109477"] # Canonical
+resource "local_file" "private_key" {
+  content         = tls_private_key.ec2_key.private_key_pem
+  filename        = "${path.module}/my-ec2-key.pem"
+  file_permission = "0600"
 }
 
 
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.vpc.default_security_group_id]
-  subnet_id              = module.vpc.private_subnets[0]
-
-  tags = {
-    Name = var.instance_name
-  }
-}
 
 
+
+# 🌐 VPC with public subnet
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.19.0"
@@ -34,9 +31,76 @@ module "vpc" {
   name = "example-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
+  azs             = ["us-west-2a", "us-west-2b"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  enable_dns_hostnames = true
+  map_public_ip_on_launch = true
+}
 
-  enable_dns_hostnames    = true
+# 🔐 Security group to allow SSH + HTTP + 8080
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Allow SSH and HTTP/8080"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # replace with your IP in production
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "App on 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-sg"
+  }
+}
+
+# 🖥️ EC2 instance
+resource "aws_instance" "app_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  subnet_id              = module.vpc.public_subnets[0]
+  key_name               = aws_key_pair.ec2_key.key_name
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "my-ec2-instance"
+  }
+}
+
+# 🔎 Get latest Ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,4 @@
-
 output "instance_hostname" {
   description = "Private DNS name of the EC2 instance."
-  value       = aws_instance.app_server.private_dns
+  value       = aws_instance.app_server.public_dns
 }
